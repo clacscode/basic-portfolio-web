88c83ac81beec02dca3bd99ac2f537ab12e134be
Claudio Sepúlveda <claud.sepulved@gmail.com>
2023-09-25 01:15:52 -0300

    Add functionality to menu button

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function toggleActive() {
     title.classList.toggle('active');
 }
 setInterval(toggleActive, 900);
+
+const profile = document.getElementById('profile');
+const content = document.getElementById('content');
+const project = document.getElementById('project');
+const profileItem = document.getElementById('profile-item');
+const contentItem = document.getElementById('content-item');
+const projectItem = document.getElementById('project-item');
+
+function showItem(btn, element) {
+    btn.addEventListener('click', () => {
+        profileItem.style.display = 'none';
+        contentItem.style.display = 'none';
+        projectItem.style.display = 'none';
+        element.style.display = 'flex';
+    });
+}
+
+showItem(profile, profileItem);
+showItem(content, contentItem);
+showItem(project, projectItem);
